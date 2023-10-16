8227e53da1f0d2b42f19bb5bdc049c622cb68792
sofiabovkush <148110448+sofiabovkush@users.noreply.github.com>
2023-10-16 20:37:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            float c;
+
+            a = Console.ReadLine();
+            c = Convert.ToSingle(a);
+
+            Console.WriteLine(2 * (Math.Pow(Math.Sin((3 * Math.PI) - (2 * c)), 2) - Math.Pow(Math.Cos((5 * Math.PI) - (2 * c)), 2)));
+
+            Console.ReadLine();
+        }
+    }
+}
